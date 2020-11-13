e63b16786a9953c261243680833f1928427b4cf5
Joachim Koch <joachimvkoch@gmail.com>
2020-11-13 12:30:57 +0100

    refactor(refactored and styled search component): refactored and styled search component

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,51 +1,92 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import './Search.css';
 
 function Search({ handleSearchForm }) {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const init = {
+    author: '',
+    title: '',
+    journal: '',
+    abstract: '',
+  };
+  const [fields, setFields] = useState(init);
   const [toGraph, setToGraph] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleSearchForm(title, author);
-    setTitle(() => '');
-    setAuthor(() => '');
+    handleSearchForm(fields.title, fields.author);
+    setFields(() => init);
     setToGraph(() => true);
   };
 
-  const handleTitleChange = (e) => {
-    setTitle(() => e.target.value);
-  };
-
-  const handleAuthorChange = (e) => {
-    setAuthor(() => e.target.value);
+  const handleChange = (e) => {
+    setFields((prev) => {
+      const newState = { ...prev };
+      newState[e.target.name] = e.target.value;
+      return newState;
+    });
   };
 
   return (
     <>
       {toGraph ? <Redirect to="/graph" /> : null}
       <form className="search-form" onSubmit={handleSubmit}>
-        <label htmlFor="search-input-title" className="search-label-title">
-          Title
-        </label>
-        <input
-          type="text"
-          className="search-input-title"
-          value={title}
-          onChange={handleTitleChange}
-        />
-        <label htmlFor="search-input-author" className="search-label-author">
-          Author
-        </label>
-        <input
-          type="text"
-          className="search-input-author"
-          name="search-input-author"
-          value={author}
-          onChange={handleAuthorChange}
-        />
-        <input type="submit" className="search-submit-graph" value="Search" />
+        <h1>Search</h1>
+        <div className="search-title">
+          <label htmlFor="search-input-title" className="search-label-title">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            className="search-input-title"
+            value={fields.title}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="search-author">
+          <label htmlFor="search-input-author" className="search-label-author">
+            Author
+          </label>
+          <input
+            type="text"
+            name="author"
+            className="search-input-author"
+            value={fields.author}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="search-journal">
+          <label
+            htmlFor="search-input-journal"
+            className="search-label-journal"
+          >
+            Journal
+          </label>
+          <input
+            type="text"
+            name="journal"
+            className="search-input-journal"
+            value={fields.journal}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="search-abstract">
+          <label
+            htmlFor="search-input-abstract"
+            className="search-label-abstract"
+          >
+            Abstract
+          </label>
+          <input
+            type="text"
+            name="abstract"
+            className="search-input-abstract"
+            value={fields.abstract}
+            onChange={handleChange}
+          />
+        </div>
+        <input type="submit" className="search-submit" value="Search" />
       </form>
     </>
   );
