9de1e9e81b9187ccd051ad6006a2d8760e275b9f
maxeneg <maxene.graze@gmail.com>
2020-11-25 08:54:39 -0800

    chore(graph): removed debugs

--- client/src/components/Graph/__test__/Graph.test.tsx
+++ client/src/components/Graph/__test__/Graph.test.tsx
@@ -12,9 +12,6 @@ import {
 import Graph from '../Graph';
 import { mockData } from './mockData';
 
-//getbytestid & provide svg testid: graph-svg to know if rendered
-//need to create graph & expect nodes
-//getbyquery & query circle
 afterEach(cleanup);
 
 const mockDimensions = { width: 1428, height: 714 };
@@ -31,28 +28,21 @@ describe('Graph', () => {
   });
   test('renders Graph component', () => {
     expect(screen.getByTestId('graph-svg')).toBeInTheDocument();
-    screen.debug();
   });
 
   test('renders articles upon node click', () => {
-    //   expect(screen.queryAllByText('Expand graph')).not.toBeInTheDocument();
     const circle = document.querySelector('circle');
-    fireEvent.click(circle); //vs user event
+    fireEvent.click(circle);
 
-    // screen.getByRole('button');
     expect(screen.getByText('show details')).toBeInTheDocument();
-    // expect(screen.getByText('show details')).toBeInTheDocument();
-    screen.debug();
   });
 
   test('renders subject legend', () => {
     expect(
       screen.getByText('High Energy Physics - Phenomenology')
     ).toBeInTheDocument();
-    screen.debug();
   });
   test('renders collaborator legend', () => {
     expect(screen.getByText('1 collaborator')).toBeInTheDocument();
-    screen.debug();
   });
 });
